923a524d323d55799964dbd8fde94def1c4ca429
pijushghorai <pghorai098@gmail.com>
2024-01-06 01:54:02 +0530

    adding bottom project bar and write javascript for image hover animation

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+let a = document.querySelector(".project-bar-container")
+let btmImg = document.querySelector("#bottom-img")
+a.addEventListener("mouseenter", function(){
+    btmImg.style.display = "block"
+})
+a.addEventListener("mouseleave", function(){
+    btmImg.style.display = "none"
+})
+
+let projectBar = document.querySelectorAll("#project-bar1")
+projectBar.forEach(function(i){
+    i.addEventListener("mouseenter", function(){
+        let image = i.getAttribute("data-img")
+        btmImg.style.backgroundImage = `url(${image})`
+    })
+})
