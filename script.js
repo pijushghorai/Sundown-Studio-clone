656a3a995b637d3c9654ab6bbf4973c5bf00b6fa
pijushghorai <pghorai098@gmail.com>
2024-01-07 21:25:39 +0530

    add footersection and some work swiper section

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
-
-let a = document.querySelector(".project-bar-container")
-let btmImg = document.querySelector("#bottom-img")
-a.addEventListener("mouseenter", function(){
-    btmImg.style.display = "block"
-})
-a.addEventListener("mouseleave", function(){
-    btmImg.style.display = "none"
-})
-
-let projectBar = document.querySelectorAll("#project-bar1")
-projectBar.forEach(function(i){
-    i.addEventListener("mouseenter", function(){
-        let image = i.getAttribute("data-img")
-        btmImg.style.backgroundImage = `url(${image})`
+function PageAnimation() {
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('#main'),
+        smooth: true
+    });
+    
+    let a = document.querySelector(".project-bar-container")
+    let btmImg = document.querySelector("#bottom-img")
+    a.addEventListener("mouseenter", function(){
+        btmImg.style.display = "block"
     })
-})
+    a.addEventListener("mouseleave", function(){
+        btmImg.style.display = "none"
+    })
+    
+    let projectBar = document.querySelectorAll("#project-bar1")
+    projectBar.forEach(function(i){
+        i.addEventListener("mouseenter", function(){
+            let image = i.getAttribute("data-img")
+            btmImg.style.backgroundImage = `url(${image})`
+        })
+    })
+}
+
+function SwiperAnimation() {
+    let swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
+      });
+}
+
+PageAnimation()
+SwiperAnimation()
